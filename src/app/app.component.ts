cb17af1719bafcfd5b5e6dd05df5f89fd2b3c4a1
Nitesh Agarwal <nitesh@codenation.co.in>
2017-10-20 17:32:11 +0530

    listening to auth state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import firebase from 'firebase';
+import {FIREBASE_CREDS} from '../../configs/config';
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -16,7 +18,17 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      firebase.initializeApp(FIREBASE_CREDS);
     });
   }
 }
+const unsubscribe = firebase.auth().onAuthStateChanged(user =>{
+  if(!user) {
+    this.rootPage = 'LoginPage';
+    unsubscribe();
+  }else {
+    this.rootPage = HomePage;
+    unsubscribe();
+  }
+});
 
