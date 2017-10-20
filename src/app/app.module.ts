4a7315a1982049cb0b95663f91cced91e8de7463
Nitesh Agarwal <nitesh@codenation.co.in>
2017-10-20 16:44:46 +0530

    initial setup complete for TaskManager

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AuthProvider } from '../providers/auth/auth';
+import { TaskProvider } from '../providers/task/task';
+import { ProfileProvider } from '../providers/profile/profile';
+import {Camera} from '@ionic-native/camera';
 
 @NgModule({
   declarations: [
@@ -24,7 +28,11 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthProvider,
+    TaskProvider,
+    ProfileProvider,
+    Camera,
   ]
 })
 export class AppModule {}
